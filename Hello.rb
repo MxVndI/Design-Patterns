fa383b50a69395d13cf6add3b81c3990e18b6e32
MxVndI <kingknife29@gmail.com>
2024-09-13 10:56:31 +0300

    add language question, task 2

--- Hello.rb
+++ Hello.rb
@@ -1,1 +1,14 @@
-puts "Hello World!"
+puts "Hello World! " + ARGV[0]
+puts "Введите свой любимый язык программирования"
+language = $stdin.gets.chomp
+if language == "ruby"
+	puts "Ну и подлиза ты"
+else
+	case language
+		when "java" then puts "ну пойдет"
+		when "c++" then puts "ты маньяк что ли?"
+		when "kotlin" then puts "неплохо"
+		when "assembler" then puts "хахахахах"
+	end
+	puts "Ничего, скоро будет ruby"
+end
