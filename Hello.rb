34ab1e24f64b51bf2775ef10f70bf911f31dedfa
MxVndI <kingknife29@gmail.com>
2024-09-13 21:50:04 +0300

    solving 4 task

--- Hello.rb
+++ Hello.rb
@@ -1,21 +1,98 @@
-puts "Hello World! " + ARGV[0]
-puts "Введите свой любимый язык программирования"
-language = $stdin.gets.chomp
-if language == "ruby"
-	puts "Ну и подлиза ты"
-else
-	case language
-		when "java" then puts "ну пойдет"
-		when "c++" then puts "ты маньяк что ли?"
-		when "kotlin" then puts "неплохо"
-		when "assembler" then puts "хахахахах"
+def countDels number
+	countDels = 0
+	number.times do |i|
+		if (i != 0 && number % i == 0 && isMultipleThree(i))
+		countDels += 1
+		end
 	end
-	puts "Ничего, скоро будет ruby"
+	return countDels + 1
 end
-puts "Введите команду языка Ruby"
-rubyCommand = $stdin.gets.chomp
-eval(rubyCommand)
-puts "Введите команду операционной системы"
-osCommand = $stdin.gets.chomp
-Kernel.`(osCommand)
-#например можно ввести start cmd
+
+def minNechet number
+	min = 9
+	while number > 0 do
+		if (min >= (number % 10) && isNechet(number % 10))
+			min = number % 10
+		end
+		number = number / 10
+	end
+	return min
+end
+
+def isMultipleThree number
+	if (number % 3 != 0)
+		return true
+	else
+		return false
+	end
+end
+
+def isNechet number
+	if number % 2 == 1
+		return true
+	else
+		return false
+	end
+end
+
+def sumDivisors number
+	sum = 0
+	(number+1).times do |i|
+		sumNumber = sumNumber(number)
+		mulNumber = mulNumber(number)
+		if (i != 0 && number % i == 0 && isVzaimoProst(sumNumber, i) && !isVzaimoProst(mulNumber, i))
+			sum += i
+		end
+	end
+	return sum
+end
+
+def sumNumber number
+	sum = 0
+	while number > 0 do
+		sum += number % 10
+		number = number / 10
+	end
+	return sum
+end
+
+def mulNumber number
+	multiply = 1
+	while number > 0 do
+		multiply *= number % 10
+		number = number / 10
+	end
+	return multiply
+end
+
+def isVzaimoProst num1, num2
+	if (findNOD(num1, num2) == 1)
+		return true
+	else 
+		return false
+	end
+end
+
+def findNOD num1, num2
+	nod = num1 >= num2 ? num1 : num2
+	until (num1 % nod == 0) and (num2 % nod == 0)
+		nod -= 1
+	end
+	return nod
+end
+
+
+puts "Введите число"
+number = $stdin.gets.chomp
+divisors = countDels(number.to_i)
+puts "Количество делителей, не кратных 3: #{divisors}"
+
+puts "Введите число"
+number = $stdin.gets.chomp
+minNech = minNechet(number.to_i)
+puts "Минимальное нечетное число: #{minNech}"
+
+puts "Введите число"
+number = $stdin.gets.chomp
+sumDiv = sumDivisors(number.to_i)
+puts "Сумма всех делителей числа, взаимно простых с суммой цифр числа и не взаимно простых с произведением цифр числа: #{sumDiv}"
