4d47a440010d477c5e54a5c71a453159a0202d40
Владимир <158157147+MxVndI@users.noreply.github.com>
2024-09-21 08:39:23 +0300

    Add files via upload
    changed camelCase to snake_case, collected all solutions in the one commit

--- Hello.rb
+++ Hello.rb
@@ -1,17 +1,17 @@
-def countDels number
-	countDels = 0
+def count_dels number
+	count_dels = 0
 	number.times do |i|
-		if (i != 0 && number % i == 0 && isMultipleThree(i))
-		countDels += 1
+		if (i != 0 && number % i == 0 && is_multiple_three?(i))
+		count_dels += 1
 		end
 	end
-	return countDels + 1
+	return count_dels + 1
 end
 
-def minNechet number
+def min_nechet number
 	min = 9
 	while number > 0 do
-		if (min >= (number % 10) && isNechet(number % 10))
+		if (min >= (number % 10) && is_nechet?(number % 10))
 			min = number % 10
 		end
 		number = number / 10
@@ -19,7 +19,7 @@ def minNechet number
 	return min
 end
 
-def isMultipleThree number
+def is_multiple_three? number
 	if (number % 3 != 0)
 		return true
 	else
@@ -27,7 +27,7 @@ def isMultipleThree number
 	end
 end
 
-def isNechet number
+def is_nechet? number
 	if number % 2 == 1
 		return true
 	else
@@ -35,19 +35,19 @@ def isNechet number
 	end
 end
 
-def sumDivisors number
+def sum_divisors number
 	sum = 0
 	(number+1).times do |i|
-		sumNumber = sumNumber(number)
-		mulNumber = mulNumber(number)
-		if (i != 0 && number % i == 0 && isVzaimoProst(sumNumber, i) && !isVzaimoProst(mulNumber, i))
+		sum_number = sum_number(number)
+		mul_number = mul_number(number)
+		if (i != 0 && number % i == 0 && is_vzaimo_prost?(sum_number, i) && !is_vzaimo_prost?(mul_number, i))
 			sum += i
 		end
 	end
 	return sum
 end
 
-def sumNumber number
+def sum_number number
 	sum = 0
 	while number > 0 do
 		sum += number % 10
@@ -56,7 +56,7 @@ def sumNumber number
 	return sum
 end
 
-def mulNumber number
+def mul_number number
 	multiply = 1
 	while number > 0 do
 		multiply *= number % 10
@@ -65,15 +65,15 @@ def mulNumber number
 	return multiply
 end
 
-def isVzaimoProst num1, num2
-	if (findNOD(num1, num2) == 1)
+def is_vzaimo_prost? num1, num2
+	if (find_NOD(num1, num2) == 1)
 		return true
 	else 
 		return false
 	end
 end
 
-def findNOD num1, num2
+def find_NOD num1, num2
 	nod = num1 >= num2 ? num1 : num2
 	until (num1 % nod == 0) and (num2 % nod == 0)
 		nod -= 1
@@ -81,22 +81,102 @@ def findNOD num1, num2
 	return nod
 end
 
+puts "Hello World! " + ARGV[0]
+puts "Введите свой любимый язык программирования"
+language = $stdin.gets.chomp
+if language == "ruby"
+	puts "Ну и подлиза ты"
+else
+	case language
+		when "java" then puts "ну пойдет"
+		when "c++" then puts "ты маньяк что ли?"
+		when "kotlin" then puts "неплохо"
+		when "assembler" then puts "хахахахах"
+	end
+	puts "Ничего, скоро будет ruby"
+end
+
+puts "Введите команду языка Ruby"
+ruby_сommand = $stdin.gets.chomp
+eval(ruby_сommand)
+puts "Введите команду операционной системы"
+os_сommand = $stdin.gets.chomp
+Kernel.`(os_сommand)
+
+puts "Введите число"
+number = $stdin.gets.chomp
+divisors = count_dels(number.to_i)
+puts "Количество делителей, не кратных 3: #{divisors}"
+puts "Введите число"
+number = $stdin.gets.chomp
+minNech = min_nechet(number.to_i)
+puts "Минимальное нечетное число: #{minNech}"
+puts "Введите число"
+number = $stdin.gets.chomp
+sumDiv = sum_divisors(number.to_i)
+puts "Сумма всех делителей числа, взаимно простых с суммой цифр числа и не взаимно простых с произведением цифр числа: #{sumDiv}"
+
+def min_element_for(array)
+	min = array[0] 
+	for i in 1...array.size
+		if array[i] < min
+			min = array[i] 
+		end
+	end
+	return min
+end
+
+def min_element_while(array)
+	min = array[0]
+	i = 1
+	while i < array.size
+		if array[i] < min
+			min = array[i] 
+		end
+		i += 1
+	end
+	return min
+end
+
+def first_index_pos_elem_for(array)
+	for i in 0...array.size
+		if array[i] > 0
+			return i
+		end
+	end
+end
+
+def first_index_pos_elem_while(array)
+	i = 0
+	while i < array.size
+		if (array[i] > 0)
+			return i
+		end
+		i += 1
+	end
+end
+
+puts "Введите массив:"
+array=$stdin.gets.chomp.split.map {|el| el.to_i}
+puts "минимальный элемент (for): #{min_element_for(array)}"
+puts "минимальный элемент (while): #{min_element_while(array)}"
+puts "индекс первого положительного элемента (for): #{first_index_pos_elem_for(array)}"
+puts "индекс первого положительного элемента (while): #{first_index_pos_elem_while(array)}"
+
 result = 0
 file = File.open(ARGV[1])
-fileData = file.readlines
-number = fileData[0].to_i
-array = fileData[1].split.map {|el| el.to_i}
+file_data = file.readlines
+number = file_data[0].to_i
+array = file_data[1].split.map {|el| el.to_i}
 case ARGV[0]
 	when 1 
-		result = countDels(number.to_i)
+		result = count_dels(number.to_i)
 		puts "Количество делителей, не кратных 3: #{result}"
 	when 2
-		result = minNechet(number.to_i)
+		result = min_nechet(number.to_i)
 		puts "Минимальное нечетное число: #{result}"
 	when 3
-		result = sumDivisors(number.to_i)
+		result = sum_divisors(number.to_i)
 		puts "Сумма всех делителей числа, взаимно простых с суммой цифр числа и не взаимно простых с произведением цифр числа: #{result}"
 	else puts "Такого метода нет"
 end
-
-
