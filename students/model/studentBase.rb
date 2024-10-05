cfcaa9ced556558b0bf013bd7ee307a21229984c
Владимир <158157147+MxVndI@users.noreply.github.com>
2024-10-05 09:48:36 +0300

    Update studentBase.rb
    fix code style

--- students/model/studentBase.rb
+++ students/model/studentBase.rb
@@ -1,22 +1,21 @@
 require_relative '../validator/uservalidator.rb'
 
 class StudentBase
+   def initialize(id, git: nil)
+       @id = id
+       @git = git
+   end
 
-	def initialize(id, git: nil)
-		@id = id
-		@git = git
-  end
-
-	def validate()
-		if (@git != nil and (@phone != nil or @telegram != nil or @mail != nil or @contact != nil)) 
-			return true
+   def validate()
+	if (@git != nil and (@phone != nil or @telegram != nil or @mail != nil or @contact != nil)) 
+		return true
 		else return false
 		end
 	end
 	
-	def git=(value)
-		if (UserValidator.is_valid_git?(value))
-			@git = value
-		end
+    def git=(value)
+	if (UserValidator.is_valid_git?(value))
+		@git = value
 	end
+     end
 end
