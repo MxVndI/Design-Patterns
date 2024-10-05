860510c8bb9ff6caf3149b2c276bdca63f87d7db
Владимир <158157147+MxVndI@users.noreply.github.com>
2024-10-05 10:26:52 +0300

    add check has contact

--- students/model/studentBase.rb
+++ students/model/studentBase.rb
@@ -1,13 +1,13 @@
 require_relative '../validator/uservalidator.rb'
 
 class StudentBase
-   def initialize(id, git: nil)
+   def initialize(id: nil, git: nil)
        @id = id.to_i
        @git = git
    end
 
    def validate()
-	if (has_git() and (@phone != nil or @telegram != nil or @mail != nil or @contact != nil)) 
+	if (has_git() and has_contact()) 
 		return true
 	else 
 		return false
@@ -15,7 +15,11 @@ class StudentBase
     end
 
     def has_git()
-	return @git != nil    
+	return !@git.nil?    
+    end
+
+    def has_contact()
+    	return !@phone.nil? or !@telegram.nil? !@mail.nil? or !@contact.nil?
     end
 	
     def git=(value)
