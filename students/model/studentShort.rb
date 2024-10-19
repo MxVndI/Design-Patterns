84e3b1f5d0e15696f2ad38f88caecc88a0c7c528
Владимир <158157147+MxVndI@users.noreply.github.com>
2024-10-19 08:39:59 +0300

    create parses for studentShort

--- students/model/studentShort.rb
+++ students/model/studentShort.rb
@@ -1,21 +1,28 @@
 require_relative "student.rb"
 
 class StudentShort < StudentBase
-  attr_reader :id, :FIO, :git, :contact
-  
-  def initialize(student: nil, id: nil, student_info: nil)
-    if student
-      parseStudentInfo(student.get_info)
-    elsif id && student_info
-	  super(id)
-      parseStudentInfo(student_info)
-    end
+  attr_reader :id, :fio, :git, :contact
+
+  private_class_method :new
+	
+  def initialize(fio: nil, id: nil, contact: nil, git: nil)
+    super(id)
+    @fio = fio
+    @contact = contact
+    @git = git	  
   end
   
-  private def parseStudentInfo(student_info)
+  def self.parseStudentInfo(student_info)
     info = student_info.split(",")
-    @FIO = info[0]
-    self.git = info[1]
-    @contact = info[2]
+    fio = info[0]
+    git = info[1]
+    contact = info[2]
+    new(fio: fio, git: git, contact: contact)
+  end
+
+  def self.parseStudent(student)
+    if student.is_a?(Student)
+	StudentShort.parseStudentInfo(student.get_info())
+    end
   end
 end
