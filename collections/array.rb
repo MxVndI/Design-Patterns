09da6765828a053517baab844e59fb90a220f9c1
Владимир <158157147+MxVndI@users.noreply.github.com>
2024-11-09 10:32:15 +0300

    add swap_min_max lab 3 task 2

--- collections/array.rb
+++ collections/array.rb
@@ -1,7 +1,15 @@
 def is_min(arr, index)
-	yield (array.min == array.at(index))
+	yield (arr.min == arr.at(index))
+end
+
+def swap_min_max(arr)
+	arr[arr.index(arr.min)],  arr[arr.index(arr.max)] = arr[arr.index(arr.max)], arr[arr.index(arr.min)]
+	yield(arr)
 end
 
 array = gets.chomp.split.map{|el| el.to_i} 
 index = gets.to_i
 is_min(array , index) { |result| puts "Является ли минимумом? #{result}" }
+
+puts array
+swap_min_max(array) { |result| puts "Новый массив #{result}" }	
