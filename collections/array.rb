85bdf39a4162480c34109c20b90179ae9df246d6
Владимир <158157147+MxVndI@users.noreply.github.com>
2024-11-09 10:10:20 +0300

    add check is_min by index method lab 3 task 1

--- collections/array.rb
+++ collections/array.rb
@@ -1,1 +1,7 @@
+def is_min(arr, index)
+	yield (array.min == array.at(index))
+end
 
+array = gets.chomp.split.map{|el| el.to_i} 
+index = gets.to_i
+is_min(array , index) { |result| puts "Является ли минимумом? #{result}" }
