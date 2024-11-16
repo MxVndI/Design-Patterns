a33a79bd41d2ec1055c2384a0e7e7e9bd26b04bb
Владимир <158157147+MxVndI@users.noreply.github.com>
2024-11-16 09:55:29 +0300

    add lab3 task 1.3

--- collections/array.rb
+++ collections/array.rb
@@ -7,9 +7,19 @@ def swap_min_max(arr)
 	yield(arr)
 end
 
+def check_max_in_ab (array, a, b)
+	arr.max >= a && arr.max < b
+	yield(a, b)
+end
+
 array = gets.chomp.split.map{|el| el.to_i} 
 index = gets.to_i
 is_min(array , index) { |result| puts "Является ли минимумом? #{result}" }
 
 puts array
 swap_min_max(array) { |result| puts "Новый массив #{result}" }	
+
+puts array
+a = gets.to_i
+b = gets.to_i
+check_max_in_ab (array, a, b) { a, b puts " Максимум находися в интервале [#{a}, #{b}]" }	
